8a91d4ef368517b7981b7b622b609fdabccf8415
gyasifordjour <150396957+gyasifordjour@users.noreply.github.com>
2023-12-18 09:30:12 +0000

    updated files

--- server.js
+++ server.js
@@ -1,33 +1,110 @@
 const express = require('express');
-const app = express();
-const connectToDatabase = require('./services/database');
 const mongoose = require('mongoose');
-const Patient = require('./models/patient');
-const patientRoutes = require('./routes/patientRoutes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connecting to MongoDB
+mongoose.connect('mongodb://localhost/ugmc', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Patient Schema
+const patientSchema = new mongoose.Schema({
+  surname: String,
+  otherNames: String,
+  gender: String,
+  phoneNumber: String,
+  residentialAddress: String,
+  emergencyContact: {
+    name: String,
+    phone: String,
+    relationship: String,
+  },
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+// Encounter Schema
+const encounterSchema = new mongoose.Schema({
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
+  dateAndTime: { type: Date, default: Date.now },
+  encounterType: String,
+});
+
+const Encounter = mongoose.model('Encounter', encounterSchema);
+
+// Vitals Schema
+const vitalsSchema = new mongoose.Schema({
+  encounterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Encounter' },
+  bloodPressure: String,
+  temperature: Number,
+  pulse: Number,
+  spO2: Number,
+});
+
+const Vitals = mongoose.model('Vitals', vitalsSchema);
+
+// Body Parser Middleware
 app.use(express.json());
-connectToDatabase();
 
+// Routes
 
+// Register Patients
+app.post('/api/patients', async (req, res) => {
+  try {
+    const patient = new Patient(req.body);
+    await patient.save();
+    res.json({ patientId: patient._id, message: 'Patient registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering patient' });
+  }
+});
 
+// Start Encounter
+app.post('/api/encounters', async (req, res) => {
+  try {
+    const encounter = new Encounter(req.body);
+    await encounter.save();
+    res.json({ encounterId: encounter._id, message: 'Encounter started successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error starting encounter' });
+  }
+});
 
+// Submit Patient Vitals
+app.post('/api/vitals', async (req, res) => {
+  try {
+    const vitals = new Vitals(req.body);
+    await vitals.save();
+    res.json({ message: 'Vitals submitted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error submitting vitals' });
+  }
+});
 
-mongoose.
-connect('mongodb+srv://sgfordjour:l1Eu0mBQKIAH8tRr@kay.ocovk1l.mongodb.net/node-API?retryWrites=true&w=majority')
-.then(() => {
-    console.log('connected to mongoDB')
+// View List of Patients
+app.get('/api/patients', async (req, res) => {
+  try {
+    const patients = await Patient.find({}, 'patientId surname otherNames gender phoneNumber');
+    res.json({ patients });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching patients' });
+  }
+});
 
-    app.listen(3000, () => {
-        console.log('Node api is running on port 3000')
-    })
+// View Details of a Patient
+app.get('/api/patients/:patientId', async (req, res) => {
+  try {
+    const patientId = req.params.patientId;
+    const patient = await Patient.findById(patientId).populate({
+      path: 'encounters',
+      populate: { path: 'vitals' },
+    });
+    res.json(patient);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching patient details' });
+  }
+});
 
-}).catch((error) =>{
-    console.log(error)
-
-})
-
-app.use('/api/patients', patientRoutes);
-
-
-app.listen(3000, () => {
-    console.log(`Server is running `);
-  });
+// Start Server
+app.listen(PORT, () => {
+  console.log(Server is running on port ${PORT});
+});
